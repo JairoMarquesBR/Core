34a7d1b16e8f71d793db8fef2f53b8b6e4e210b6
Francisco Jairo Marques dos Santos <jairomarques.servico@gmail.com>
2022-05-28 00:22:36 -0300

    Toast adicionada com um icone no console

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,26 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(public toastController: ToastController) {}
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      header: 'Console indisponivel',
+      message: 'Função em desenvolvimento',
+      position: 'bottom',
+      buttons: [
+         {
+          icon: 'radio-button-off-sharp',
+          role: 'cancel',
+          handler: () => {
+            console.log('Toast fechada');
+          }
+        }
+      ]
+    });
+    await toast.present();
+
+    const { role } = await toast.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
 
 }
